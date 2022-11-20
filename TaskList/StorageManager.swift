005cd4ec43ec671bcb273902d1db8be8942d8fef
albinalega <lega.albina@mail.ru>
2022-11-20 16:29:44 +0700

    Created StorageManager and deleted CoreData from AppDelegate

---
+++ TaskList/StorageManager.swift
@@ -0,0 +1,38 @@
+//
+//  StorageManager.swift
+//  TaskList
+//
+//  Created by Альбина Лега on 20.11.2022.
+//
+
+import Foundation
+import CoreData
+
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private init() {}
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TaskList")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
